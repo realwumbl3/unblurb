2a24f7bdaaf70fce27f81b3191160f1560ef3444
realwumbl3 <wumbl3@gmail.com>
2024-04-06 15:45:14 -0400

    works 100% of the time now

--- main.js
+++ main.js
@@ -1,10 +1,28 @@
-function observe(container, element, func) {
+function observe(container, element, func, once) {
     'use strict';
+    if (once === undefined) once = false;
     try {
         const no = new MutationObserver((mutationsList) => {
             for (const mutation of mutationsList) if (mutation.type === 'childList') {
-                for (const node of mutation.addedNodes) if (typeof node?.querySelectorAll === "function" && node.querySelectorAll(element).length > 0)
-                    func(node.querySelectorAll(element))
+                for (const node of mutation.addedNodes) {
+                    if (!node || node.nodeType !== 1) continue
+                    if (node.matches(element)) {
+                        func([node])
+                        if (once) {
+                            no.disconnect()
+                            continue
+                        }
+                    }
+                    if (typeof node?.querySelectorAll === "function" && node.querySelectorAll(element).length > 0) {
+                        const query = node.querySelectorAll(element)
+                        if (!query) continue
+                        func(query)
+                        if (once) {
+                            no.disconnect()
+                            continue
+                        }
+                    }
+                }
             }
         })
         no.observe(container, { childList: true, subtree: true })
@@ -19,33 +37,33 @@ async function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-observe(document.body, '[role="main"]', _ => {
-    // console.log("Main >", _)
+observe(document, 'main', _ => {
+    console.log("[UnBlurd] Observing <main/>", _[0])
     observe(_[0], '[role="article"],[role="listitem"]', async tweets => {
-        for (const tweet of [...tweets]) {
-            const role = tweet.getAttribute('role')
-            if (role === "article") {
-                // console.log("Tweet Article >", tweet)
-                const media_warning = tweet.querySelector('.css-175oi2r.r-1kihuf0.r-1e081e0')
-                if (!media_warning) return false
-                // console.log("Tweet w Media Warning >", media_warning)
-                clickTheUnblurButton(media_warning)
-            } else if (role === "listitem") {
-                // console.log("Tweet Grid Item >", tweet)
-                clickTheUnblurButton(tweet)
+        try {
+            for (const tweet of [...tweets]) {
+                const role = tweet.getAttribute('role')
+                if (role === "article") {
+                    const media_warning = tweet.querySelector('.css-175oi2r.r-1kihuf0.r-1e081e0')
+                    if (media_warning) clickTheUnblurButton(media_warning)
+                }
+                if (role === "listitem") {
+                    clickTheUnblurButton(tweet)
+                }
             }
+        } catch (err) {
+            console.error("[UnBlurd] Error", err)
+            console.log("[UnBlurd] Additional data", { tweets })
         }
     })
-})
+}, true)
 
 function clickTheUnblurButton(container) {
     const unblur_button = container.querySelectorAll('[role="button"]')
     if (!unblur_button) return false
-    // console.log("Unblur Button >", unblur_button)
     for (const button of [...unblur_button]) {
         const click_event = button?.click
         if (typeof click_event !== "function") return false
-        // console.log("Clicking Button >", button)
         click_event.call(button)
     }
 }
