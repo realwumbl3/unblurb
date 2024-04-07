b855d9072bba93f7820d2f1e33d7aac03d402d51
realwumbl3 <wumbl3@gmail.com>
2024-04-06 22:09:10 -0400

    :3

--- main.js
+++ main.js
@@ -1,69 +1,43 @@
-function observe(container, element, func, once) {
+const sleep = async (ms) => new Promise(resolve => setTimeout(resolve, ms));
+
+function observe(container, query, func, once) {
     'use strict';
-    if (once === undefined) once = false;
-    try {
-        const no = new MutationObserver((mutationsList) => {
-            for (const mutation of mutationsList) if (mutation.type === 'childList') {
-                for (const node of mutation.addedNodes) {
-                    if (!node || node.nodeType !== 1) continue
-                    if (node.matches(element)) {
-                        func([node])
-                        if (once) {
-                            no.disconnect()
-                            continue
-                        }
-                    }
-                    if (typeof node?.querySelectorAll === "function" && node.querySelectorAll(element).length > 0) {
-                        const query = node.querySelectorAll(element)
-                        if (!query) continue
-                        func(query)
-                        if (once) {
-                            no.disconnect()
-                            continue
-                        }
-                    }
+    once = once || false
+    const nmo = new MutationObserver((mutationsList) => {
+        for (const mutation of mutationsList) if (mutation.type === 'childList') {
+            for (const node of mutation.addedNodes) {
+                if (!node || node.nodeType !== 1) continue
+                if (node.matches(query)) {
+                    func(node)
+                    if (once) return nmo.disconnect()
+                } else if (node.querySelectorAll(query).length > 0) {
+                    for (const elem of node.querySelectorAll(query)) func(elem)
+                    if (once) return nmo.disconnect()
                 }
             }
-        })
-        no.observe(container, { childList: true, subtree: true })
-        return no
-    }
-    catch (err) {
-        console.log("err.message: " + err.message)
-    }
+        }
+    })
+    nmo.observe(container, { childList: true, subtree: true })
+    return nmo
 }
 
-async function sleep(ms) {
-    return new Promise(resolve => setTimeout(resolve, ms));
+function clickTheUnblurButton(container) {
+    const unblurButton = container.querySelectorAll('[role="button"]')
+    if (!unblurButton) return false
+    for (const button of [...unblurButton]) button?.click.call(button)
 }
 
 observe(document, 'main', _ => {
-    console.log("[UnBlurd] Observing <main/>", _[0])
-    observe(_[0], '[role="article"],[role="listitem"]', async tweets => {
+    console.log("[UnBlurd] Observing <main/>", _)
+    observe(_, '[role="article"],[role="listitem"]', async tweet => {
         try {
-            for (const tweet of [...tweets]) {
-                const role = tweet.getAttribute('role')
-                if (role === "article") {
-                    const media_warning = tweet.querySelector('.css-175oi2r.r-1kihuf0.r-1e081e0')
-                    if (media_warning) clickTheUnblurButton(media_warning)
-                }
-                if (role === "listitem") {
-                    clickTheUnblurButton(tweet)
-                }
-            }
+            const role = tweet.getAttribute('role')
+            if (role === "listitem") return clickTheUnblurButton(tweet)
+            const mediaWarning = tweet.querySelector('.css-175oi2r.r-1kihuf0.r-1e081e0')
+            if (mediaWarning) clickTheUnblurButton(mediaWarning)
         } catch (err) {
             console.error("[UnBlurd] Error", err)
-            console.log("[UnBlurd] Additional data", { tweets })
+            console.log("[UnBlurd] Err context:", { tweet })
         }
     })
 }, true)
-
-function clickTheUnblurButton(container) {
-    const unblur_button = container.querySelectorAll('[role="button"]')
-    if (!unblur_button) return false
-    for (const button of [...unblur_button]) {
-        const click_event = button?.click
-        if (typeof click_event !== "function") return false
-        click_event.call(button)
-    }
-}
